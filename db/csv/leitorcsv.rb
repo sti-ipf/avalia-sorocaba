4db65b0d8991e3b329761bf45f772d9d173d6695
Andre <andre0610@gmail.com>
2011-02-06 15:25:48 -0200

    Alterado o YML

--- db/csv/leitorcsv.rb
+++ db/csv/leitorcsv.rb
@@ -75,9 +75,20 @@ csv_files.each do |group|
 
 end
 
+#Atualizando escolas que nao foram detectadas
 
 
-group_infantil
+csv_files=[
+["infantil.csv","Infantil"],
+["infantil+EMEF integral.csv","Infantil + EMEF Integral"],
+["infantil+EMEF parcial.csv","Infantil + EMEF Parcial"],
+["EMEF Parcial.csv","EMEF Parcial"],
+["EMEF integral.csv","EMEF Integral"],
+["EMEF + medio.csv","EMEF + Medio"]
+]
+
+
+group_infantil=Group.find_by_name(csv_files[0][1])
 update_group_institution(9,group_infantil)
 update_group_institution(18,group_infantil)
 update_group_institution(27,group_infantil)
@@ -85,27 +96,51 @@ update_group_institution(30,group_infantil)
 update_group_institution(33,group_infantil)
 update_group_institution(36,group_infantil)
 update_group_institution(118,group_infantil)
+update_group_institution(46,group_infantil)
+update_group_institution(53,group_infantil)
+update_group_institution(54,group_infantil)
+update_group_institution(56,group_infantil)
+update_group_institution(60,group_infantil)
+update_group_institution(74,group_infantil)
+update_group_institution(79,group_infantil)
+update_group_institution(11,group_infantil)
+update_group_institution(81,group_infantil)
+update_group_institution(84,group_infantil)
+update_group_institution(87,group_infantil)
 
 
+group_infantil_emef_integral=Group.find_by_name(csv_files[1][1])
+update_group_institution(94,group_infantil_emef_integral)
+update_group_institution(28,group_infantil_emef_integral)
 
-group_emef_integral
+group_infantil_emef_parcial=Group.find_by_name(csv_files[2][1])
+update_group_institution(104,group_infantil_emef_parcial)
+update_group_institution(105,group_infantil_emef_parcial)
+
+group_emef_parcial=Group.find_by_name(csv_files[3][1])
+update_group_institution(112,group_infantil_emef_parcial)
+
+group_emef_integral=Group.find_by_name(csv_files[4][1])
 update_group_institution(335,group_emef_integral)
 update_group_institution(362,group_emef_integral)
 update_group_institution(124,group_emef_integral)
 update_group_institution(125,group_emef_integral)
+update_group_institution(218,group_emef_integral)
+update_group_institution(245,group_emef_integral)
+update_group_institution(133,group_emef_integral)
+update_group_institution(115,group_emef_integral)
 
+
+group_emef_medio=Group.find_by_name(csv_files[5][1])
+update_group_institution(121,group_emef_medio)
+update_group_institution(122,group_emef_medio)
 
 
 
 
 #Instituicoes sem grupos
 #- 134 e 251 - Antonio Carlos de Barros
-#- 159 e 276 -
-
-
-
-
-
-
-#Atualizando escolas que nao foram detectadas
+#- 159 e 276 - Elvira Nani Monteiro
+#- 271, 154, 360 e 243 | Luiz de Sanctis
+#- 285, 361 e 168 | Luiz Ribeiro
 
