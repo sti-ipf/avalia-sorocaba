0eff13aeb88bda4ae176dc9c77dc16347a153495
Andre <andre0610@gmail.com>
2011-02-08 14:13:18 -0200

    Alteração para manter sempre da mesma cor as barras do ano

--- lib/uni_freire/graphics/indicadores.rb
+++ lib/uni_freire/graphics/indicadores.rb
@@ -1,24 +1,26 @@
 module UniFreire
   module Graphics
     class Indicadores
-      
+
       def self.create(institution_id, dimension_id, size, title=nil)
         connection = ActiveRecord::Base.connection
         graphics = []
 
         (1..11).each do |dimension_id|
           result = connection.execute "
-            SELECT CONCAT(dimension,'.',indicator) as i, year, AVG(score) AS media 
-            FROM   comparable_answers 
+            SELECT CONCAT(dimension,'.',indicator) as i, year, AVG(score) AS media
+            FROM   comparable_answers
             WHERE  institution_id = #{institution_id} AND dimension = #{dimension_id}
-            GROUP  BY i, year; 
+            GROUP  BY i, year;
             "
           graphic = UniFreire::Graphics::Generator.new(:size => size, :title => "Dimensão #{dimension_id}")
-          graphics << graphic.generate(result)
+          colors={"2008"=>1,"2009"=>2,"2010"=>3}
+          graphics << graphic.generate(result,colors)
         end
         graphics
       end
-      
+
     end
   end
 end
+
