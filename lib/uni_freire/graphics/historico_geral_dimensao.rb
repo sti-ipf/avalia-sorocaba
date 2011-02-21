dd941b202b9c4f53d74bde99d34305e7cc37c602
Andre <andre0610@gmail.com>
2011-02-21 16:21:48 -0300

    Correções no gráfico geral de dimensões

--- lib/uni_freire/graphics/historico_geral_dimensao.rb
+++ lib/uni_freire/graphics/historico_geral_dimensao.rb
@@ -3,6 +3,16 @@ module UniFreire
     class HistoricoGeralDimensao
       def self.create( size, legend,hash_report, title=nil)
         connection = ActiveRecord::Base.connection
+        complete_clause=[]
+        dimension=1
+        hash_report[:invalid_indicators].each do |t|
+          in_clause = t.join(",")
+          complete_clause << " (dimension=#{dimension}) and (indicator in (#{in_clause})) " if in_clause.size > 0
+          dimension += 1
+        end
+        clause=""
+        clause=" where NOT (#{complete_clause.join(" or ")})" if complete_clause.size > 0
+
         result = connection.execute "
           SELECT dimension,year, avg(media) as new_media from
           (SELECT ca.dimension as dimension, ca.indicator as indicator, ca.year as year, AVG(ca.score) AS media
@@ -10,7 +20,7 @@ module UniFreire
           ON iyh.institution_id = ca.institution_id and iyh.year=ca.year
           WHERE  score > 0 and iyh.level_type = #{hash_report[:number]} and
           dimension not in (#{hash_report[:invalid_dimensions]})
-          GROUP BY dimension, indicator, year) a GROUP BY dimension,year"
+          GROUP BY dimension, indicator, year) a #{clause} GROUP BY dimension,year"
         graphic = UniFreire::Graphics::Generator.new(:size => size, :title => title, :marker_font_size => 12,
                                 :no_data_message=> "\n Não há dados \npara esta \n dimensão")
         graphic.generate(result,legend,"historico_geral_dimensao_#{hash_report[:number]}")
